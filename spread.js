60095ca8e08ff7bc628cd65182d9a054776845f1
tsegaye abadi <tsegaye.abadi15@gmail.com>
2024-05-03 01:09:03 -0400

    first draft

---
+++ spread.js
@@ -0,0 +1,22 @@
+//combined
+const fruit1=['banana','orange']
+const fruit2=['avecado','lemon']
+const combined=[...fruit1,...fruit2]
+console.log(combined)
+
+const addnumbers=[1,2,3]
+function sum(a,b,c){
+    return a+b+c;
+}
+const Result = sum(...addnumbers)
+console.log(Result);
+
+function mul(...numbers){
+    return numbers.reduce((total,num)=>total * num, 1);
+
+}
+const result = mul(1,2,3,4,5);
+console.log(result)
+const colors=['red','yellow','green']
+const copyofcolors=[...colors]
+console.log(copyofcolors)
